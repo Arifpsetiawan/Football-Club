3e674eb5d37f599262808487a006b3dafd6c8a34
Arifpsetiawan <arif.p.setiawan.9@gmail.com>
2018-09-02 21:31:44 +0700

    Update Again

--- app/src/main/java/com/dicoding/footballclub/DetailClub.kt
+++ app/src/main/java/com/dicoding/footballclub/DetailClub.kt
@@ -2,24 +2,32 @@ package com.dicoding.footballclub
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.widget.ImageView
 import android.widget.TextView
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.padding
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import com.bumptech.glide.Glide
+import org.jetbrains.anko.*
 
 class DetailClub : AppCompatActivity(){
     private var detail : String = ""
     private lateinit var detailTextView : TextView
+    private lateinit var detailImageView : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         verticalLayout{
             padding = dip(16)
+            detailImageView = imageView{}.lparams{
+                width = dip(75)
+                height = dip(75)
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
             detailTextView = textView()
         }
 
         val intent = intent
         detail = intent.getStringExtra("detail")
+        val image = intent.getIntExtra("image",0)
+        Glide.with(this).load(image).into(detailImageView)
         detailTextView.text = detail
     }
 }
--- app/src/main/java/com/dicoding/footballclub/MainActivity.kt
+++ app/src/main/java/com/dicoding/footballclub/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dicoding.footballclub
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -15,12 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = findViewById<RecyclerView>(R.id.list_club)
         initData()
 
         list_club.layoutManager = LinearLayoutManager(this)
         list_club.adapter = RecyclerViewAdapter(this, items){
-            startActivity<DetailClub>("detail" to "detail")
+            startActivity<DetailClub>("detail" to "${it.detail}","image" to it.image)
         }
     }
 
@@ -31,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         items.clear()
         for (i in name.indices) {
             items.add(ClubItem(name[i],
-                    image.getResourceId(i, 0)))
+                    image.getResourceId(i, 0),detail[i]))
         }
 
         //Recycle the typed array
